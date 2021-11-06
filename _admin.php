4aafa5128f4421d3cd88437a7f800e38bf32a4d5
Jean-Christian Denis <contact@jcdenis.fr>
2021-11-06 10:19:13 +0100

    add favicon test on blog pref form

--- _admin.php
+++ _admin.php
@@ -19,17 +19,33 @@ $core->addBehavior('adminBeforeBlogSettingsUpdate', ['adminSimplyFavicon', 'admi
 
 class adminSimplyFavicon
 {
+    public static $extensions = ['ico', 'png', 'bmp', 'gif', 'jpg', 'mng'];
+
     public static function adminBlogPreferencesForm($core, $blog_settings)
     {
+        $exists = [];
+        $path   = path::fullFromRoot((string) $blog_settings->system->public_path, DC_ROOT);
+        foreach (self::$extensions as $ext) {
+            if (file_exists($path . '/favicon.' . $ext)) {
+                $exists[] = sprintf('<li title="%s">%s</li>', $path . '/favicon.' . $ext, 'favicon.' . $ext);
+            }
+        }
+
         echo
-        '<div class="fieldset"><h4 id="simply_favicon_params">Favicon</h4>' .
+        '<div class="fieldset clear"><h4 id="simply_favicon_params">Favicon</h4>' .
+        '<div class="two-cols"><div class="col">' .
         '<p><label class="classic">' .
         form::checkbox('simply_favicon', '1', (bool) $blog_settings->system->simply_favicon) .
         __('Enable "Simply favicon" extension') . '</label></p>' .
         '<p class="form-note">' .
         __("You must place an image called favicon.png or .jpg or .ico into your blog's public directory.") .
-        '</p>' .
-        '</div>';
+        '</p></div><div class="col">' .
+        (
+            empty($exists) ?
+            '<p>' . __('There are no favicon in blog public directory') . '</p>' :
+            '<p>' . __('Current favicons:') . '</p><ul class="nice">' . implode($exists) . '</ul>'
+        ) .
+        '</div></div><br class="clear" /></div>';
     }
 
     public static function adminBeforeBlogSettingsUpdate($blog_settings)
