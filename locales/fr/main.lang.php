0a36892d65bb3743b7b7fc33556ca1e71a77e5a8
Jean-Christian Denis <contact@jcdenis.fr>
2026-05-26 07:58:19 +0200

    typo

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -12,7 +12,7 @@
 use Dotclear\Helper\L10n;
 
 L10n::$locales['Enable favorite icon']                                                                           = 'Activer l\'icône de favori';
-L10n::$locales['You must place an image called favicon.png or .jpg or .ico into your blog\'s public directory.'] = 'Vous devez placer une image nommée favicon.png, .jpg ou .ico dans le répertoire publique du blog.';
+L10n::$locales['You must place an image called favicon.png or .jpg or .ico into your blog\'s public directory.'] = 'Vous devez placer une image nommée favicon.png, .jpg ou .ico dans le répertoire public du blog.';
 L10n::$locales['There are no favicon in blog public directory']                                                  = 'Il n\'y a pas de Favicon dans le répertoire public du blog.';
 L10n::$locales['Current favicons:']                                                                              = 'Favicons actuels :';
 L10n::$locales['Multi-agents favicon']                                                                           = 'Favicon multi-agents';
